d601f6b7a010658d816a8448f903fd672050244a
sleepyhazzzel <145089294+sleepyhazzzel@users.noreply.github.com>
2024-02-28 08:25:52 +0000

    Deploying to gh-pages from @ sleepyhazzzel/vet-front@c920e7558be9d6f26e4bf968a0c8f93f0db17fe0 🚀

---
+++ assets/AppointmentLayout-4304aaf1.js
@@ -0,0 +1,1 @@
+import{_ as o,a3 as t,a4 as n,a6 as c}from"./index-c18772c7.js";const r={};function a(s,_){const e=t("RouterView");return n(),c(e)}const f=o(r,[["render",a]]);export{f as default};
--- assets/MedicalLayout-0cd90108.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,a3 as t,a4 as c,a6 as a}from"./index-fa1a0a70.js";const n={};function r(s,_){const e=t("RouterView");return c(),a(e)}const i=o(n,[["render",r]]);export{i as default};
